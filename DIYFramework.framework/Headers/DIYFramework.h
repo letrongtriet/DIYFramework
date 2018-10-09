e864ced88caac4365a1c81f1e458a9dfa79ce13f
Triet Le <trile@nets.eu>
2018-10-09 10:18:19 +0300

    First commit

---
+++ DIYFramework.framework/Headers/DIYFramework.h
@@ -0,0 +1,19 @@
+//
+//  DIYFramework.h
+//  DIYFramework
+//
+//  Created by Trong Triet Le on 09/10/2018.
+//  Copyright © 2018 Trong Triet Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DIYFramework.
+FOUNDATION_EXPORT double DIYFrameworkVersionNumber;
+
+//! Project version string for DIYFramework.
+FOUNDATION_EXPORT const unsigned char DIYFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DIYFramework/PublicHeader.h>
+
+
